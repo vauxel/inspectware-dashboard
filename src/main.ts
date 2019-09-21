eb1def3cfd077cd52581bf8ca832dd1b63cde782
vauxel <vauxel@protonmail.com>
2019-09-21 17:32:56 -0400

    Set default iView locale to en-US

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./registerServiceWorker";
-import iView from 'iview';
+import iView from "iview";
+import locale from "iview/dist/locale/en-US";
 
 Vue.config.productionTip = false;
 
-Vue.use(iView);
+Vue.use(iView, { locale });
 
 new Vue({
 	router,
