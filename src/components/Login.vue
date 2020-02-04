76e3acd19fa402033f93e4111f9ef7009b790616
vauxel <vauxel@protonmail.com>
2020-02-03 20:39:36 -0500

    Improved login styling and restyled iView labels

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -2,39 +2,39 @@
 	<div class="login-container">
 		<div class="login-box">
 			<div v-show="!affiliation">
-				<div class="text-center mb-5 mt-4">
-					<h1 class="h4 text-gray-900">I Am A ...</h1>
-					<div>Choose your affiliation from the options below</div>
+				<div class="login-header">
+					<div class="login-header-title">I Am A ...</div>
+					<div class="login-header-subtitle">Choose your affiliation from the options below</div>
 				</div>
-				<div class="flex justify-around">
-					<button class="affiliation-btn" @click="setHIAffiliation">
+				<div class="login-affiliations">
+					<button class="login-affiliation-btn" @click="setHIAffiliation">
 						<i class="fas fa-3x fa-hard-hat"></i>
-						Home Inspector
+						<span>Home Inspector</span>
 					</button>
-					<button class="affiliation-btn" @click="setClientAffiliation">
+					<button class="login-affiliation-btn" @click="setClientAffiliation">
 						<i class="fas fa-3x fa-user"></i>
-						Client
+						<span>Client</span>
 					</button>
-					<button class="affiliation-btn" @click="setREAAffiliation">
+					<button class="login-affiliation-btn" @click="setREAAffiliation">
 						<i class="fas fa-3x fa-sign"></i>
-						Real Estate Agent
+						<span>Real Estate Agent</span>
 					</button>
 				</div>
 			</div>
 			<div v-show="affiliation">
-				<div class="text-center mb-5 mt-4">
-					<i v-if="affiliation == 'inspector'" class="fas fa-hard-hat fa-4x mb-4"></i>
-					<i v-if="affiliation == 'client'" class="fas fa-user fa-4x mb-4"></i>
-					<i v-if="affiliation == 'realtor'" class="fas fa-sign fa-4x mb-4"></i>
-					<h1 class="h4 text-gray-900">I Am A: <i>{{ affiliationName }}</i></h1>
-					<span class="text-muted font-weight-light">Enter your login credentials below to access your account</span>
+				<div class="login-header">
+					<i v-if="affiliation == 'inspector'" class="fas fa-hard-hat fa-3x login-affiliation-badge"></i>
+					<i v-if="affiliation == 'client'" class="fas fa-user fa-3x login-affiliation-badge"></i>
+					<i v-if="affiliation == 'realtor'" class="fas fa-sign fa-3x login-affiliation-badge"></i>
+					<div class="login-header-title">I Am A: {{ affiliationName }}</div>
+					<div class="login-header-subtitle">Enter your login credentials below to access your account</div>
 				</div>
-				<Form ref="loginForm" :model="loginInfo" :rules="loginRules">
+				<Form ref="loginForm" :model="loginInfo" :rules="loginRules" @submit="login">
 					<FormItem label="Email Address" prop="email">
-						<Input type="email" v-model="loginInfo.email" placeholder="Enter your email address"></Input>
+						<Input type="email" v-model="loginInfo.email" placeholder="Enter your email address" @keyup.enter.native="login"/>
 					</FormItem>
 					<FormItem label="Password" prop="password">
-						<Input type="password" v-model="loginInfo.password" placeholder="Enter your password"></Input>
+						<Input type="password" v-model="loginInfo.password" placeholder="Enter your password" @keyup.enter.native="login"/>
 					</FormItem>
 					<FormItem>
 						<Button type="primary" long @click="login">Log In</Button>
@@ -116,42 +116,112 @@
 	.login-container {
 		width: 100%;
 		height: 100%;
+		position: relative;
 		display: flex;
 		align-items: center;
 		justify-content: center;
 		background-color: $color_primary;
 
+		&::after {
+			content: '';
+			background-image: url("/img/loginbg.jpg");
+			background-size: cover;
+			opacity: 0.25;
+			position: absolute;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+		}
+
 		.login-box {
+			position: relative;
+			z-index: 1;
 			flex-grow: 1;
-			max-width: 500px;
-			padding: 30px;
+			max-width: 550px;
+			padding: 4rem;
+			margin: 4rem;
 			background-color: $color_white;
-			box-shadow: $shadow-1;
+			box-shadow: $shadow-2;
 			border-radius: 5px;
 
-			.affiliation-btn {
-				flex: 1;
+			&::before {
+				content: "inspectware";
+				position: absolute;
+				top: -3rem;
+				left: 50%;
+				transform: translateX(-50%);
+				font-weight: $font-weight_bold;
+				font-style: italic;
+				font-size: $font-size_xl;
+				color: $font_color_dark;
+				background-color: $color_white;
+				padding: 1rem 2rem;
+				border-radius: 10px;
+				box-shadow: $shadow-1;
+			}
+		}
+
+		.login-header {
+			margin-bottom: 3rem;
+			padding-bottom: 2rem;
+			border-bottom: 2px solid $color-grey-3;
+
+			.login-header-title {
+				font-weight: $font-weight_bold;
+				font-size: $font-size_4xl;
+			}
+
+			.login-header-subtitle {
+				font-style: italic;
+			}
+		}
+
+		.login-affiliations {
+			.login-affiliation-btn {
+				position: relative;
+				width: 100%;
 				background: $color_primary;
 				border-radius: 5px;
-				color: $color_grey-1;
-				padding: 20px 10px;
+				color: $font_color_light;
+				padding: 1rem 0;
 				margin: 0px 5px;
 				font-weight: 600;
 				border: 2px solid $color_primary-darker;
-				transition: all 150ms ease-in-out;
 
 				&:hover {
 					background-color: $color_primary-darker;
 					box-shadow: $shadow-2;
+					cursor: pointer;
+				}
+
+				&:not(:last-of-type) {
+					margin-bottom: 1rem;
 				}
 
 				.fas {
-					display: block;
-					padding-bottom: 10px;
-					margin-bottom: 5px;
-					border-bottom: 2px solid $color_grey-1;
+					float: right;
+					margin-right: 2rem;
+				}
+
+				span {
+					position: absolute;
+					top: 50%;
+					left: 2rem;
+					transform: translateY(-50%);
 				}
 			}
+		}
+
+		.login-affiliation-badge {
+			position: absolute;
+			top: -3rem;
+			left: -3rem;
+			background-color: $color_white;
+			padding: 1.5rem;
+			box-shadow: $shadow-1;
+			border-radius: 50%;
+			box-sizing: content-box;
 		}
 	}
 
